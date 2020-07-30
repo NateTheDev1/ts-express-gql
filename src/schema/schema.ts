b3d3088a8978e6c4507935933de0e40d099ec76a
NateTheDev1 <nthnlrichards@gmail.com>
2020-07-30 16:42:37 -0400

    Setting company schema and connecting to user

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -5,21 +5,26 @@ const {
   GraphQLSchema,
 } = require("graphql");
 
-const _ = require("lodash");
+import axios from "axios";
+
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
 
 const UserType = new GraphQLObjectType({
-  name: "user",
+  name: "User",
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: { type: CompanyType },
   },
 });
-
-const users: Object[] = [
-  { id: "23", firstName: "Nate", age: 20 },
-  { id: "47", firstName: "Eliza", age: 19 },
-];
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
@@ -28,7 +33,9 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then((response) => response.data);
       },
     },
   },
